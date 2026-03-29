da2252642bcc3a72afde9152d85cdf8f936a2b90
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-03-29 14:08:29 -0300

    feature-vaga[feat]: Adiciona o model de vaga

--- src/main/groovy/linketinder/model/Vaga.groovy
+++ src/main/groovy/linketinder/model/Vaga.groovy
@@ -1,4 +1,34 @@
 package linketinder.model
 
 class Vaga {
+
+    String descricao
+    String horario
+    String localizacao
+    String remuneracao
+    List<String> competencias
+
+
+    int idEmpresa
+
+    Vaga(String descricao, String horario, String localizacao, String remuneracao, List<String> competencias, int idEmpresa) {
+        this.descricao = descricao
+        this.horario = horario
+        this.localizacao = localizacao
+        this.remuneracao = remuneracao
+        this.competencias = competencias
+        this.idEmpresa = idEmpresa
+    }
+
+    @Override
+    String toString() {
+        String compTexto = (competencias == null || competencias.isEmpty()) ?
+                "sem competências cadastradas" : competencias.join(", ")
+
+        return "Descrição: " + descricao +
+                "\nHorário: " + horario +
+                "\nLocalização: " + localizacao +
+                "\nRemuneração: " + remuneracao +
+                "\nCompetências: " + compTexto
+    }
 }
