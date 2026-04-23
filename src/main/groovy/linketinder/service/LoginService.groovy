5b7cb815ccea3907a9c5ecd0409acf79af4ef1ab
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-04-23 18:11:48 -0300

    refactor(service): melhora nomes de variáveis e extrai adicionarLike no MatchService

--- src/main/groovy/linketinder/service/LoginService.groovy
+++ src/main/groovy/linketinder/service/LoginService.groovy
@@ -6,17 +6,11 @@ import linketinder.model.Empresa
 class LoginService {
 
     static Object realizarLogin(String email, String senha) {
+        Candidato candidato = CandidatoService.listar().find { it.email == email && it.senha == senha }
+        if (candidato) return candidato
 
-
-        def candidato = CandidatoService.listar().find { it.email == email && it.senha == senha }
-        if (candidato) {
-            return candidato
-        }
-
-        def empresa = EmpresaService.listar().find { it.email == email && it.senha == senha }
-        if (empresa) {
-            return empresa
-        }
+        Empresa empresa = EmpresaService.listar().find { it.email == email && it.senha == senha }
+        if (empresa) return empresa
 
         return null
     }
